66407340a43bd726ab37f2fe5c8438e7d02f11d4
Calvin Neo <calvinneo1995@gmail.com>
2021-12-05 21:03:24 +0800

    Refactor
    Signed-off-by: Calvin Neo <calvinneo1995@gmail.com>

--- TransportSimulator/src/vehicle.rs
+++ TransportSimulator/src/vehicle.rs
@@ -16,6 +16,12 @@ pub struct GotoIntention {
     pub to: Location,
 }
 
+// For every vehicle in `running_vehicles`, if a vehicle is fast enough to travel through multiple roads in one tick,
+// it should also be added into other pending lists.
+// By this, we can exactly know which vehicles can pass traffic light,
+// and study interaction(like traffic jam) between those vehicles.
+// However, due to unpredictable traffic light, and vehicle interaction, vehicles may not actually present.
+// As a result, we can find some road's `pending_vehicle` is dependent on other roads.
 pub enum Intention {
     Die,
     Goto(GotoIntention),
@@ -28,9 +34,12 @@ pub trait Vehicle {
     // In this tick, we can go this much distance.
     fn get_left_equivalent_distance(&self) -> Distance;
     fn go_distance(&mut self, distance: Distance);
-    fn elaspe_distance(&mut self, delta_tso: i64);
+    fn blocked_by(&mut self, delta_tso: i64);
     fn new_tick(&mut self, tso: i64);
     fn get_id(&self) -> i32;
+    // blocked time + running time = time elapsed since tick start
+    fn get_blocked_time(&self) -> f64;
+    fn get_running_time(&self) -> f64;
 }
 
 pub struct EndToEndCar {
@@ -41,6 +50,8 @@ pub struct EndToEndCar {
     // Vehicle can be blocked before traffic light for ticks.
     // Ticks will be translated into decreasing distance, to avoid handling float.
     left_equivalent_distance: Distance,
+    blocked_time: f64,
+    running_time: f64,
 }
 
 impl Vehicle for EndToEndCar {
@@ -54,9 +65,11 @@ impl Vehicle for EndToEndCar {
         self.left_equivalent_distance
     }
     fn go_distance(&mut self, distance: Distance) {
+        self.running_time += distance as f64 / self.speed as f64;
         self.left_equivalent_distance -= distance;
     }
-    fn elaspe_distance(&mut self, delta_tso: i64) {
+    fn blocked_by(&mut self, delta_tso: i64) {
+        self.blocked_time += delta_tso as f64;
         self.left_equivalent_distance -= delta_tso * self.speed;
     }
     fn new_tick(&mut self, total_tsos: i64) {
@@ -64,5 +77,11 @@ impl Vehicle for EndToEndCar {
     }
     fn get_id(&self) -> i32 {
         self.id
+    }
+    fn get_blocked_time(&self) -> f64 {
+        self.blocked_time
+    }
+    fn get_running_time(&self) -> f64 {
+        self.running_time
     }
 }
